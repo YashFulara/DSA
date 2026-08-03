c64c26419de14011c2c634aafccda6877bd834ab
Yashfulara <trollj9032@gmail.com>
2026-08-03 21:15:06 +0530

    60 questions done

--- Pattern/Q60.java
+++ Pattern/Q60.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
-public class Q60{
-    public static void main(String[] args) {
-     Scanner input=new Scanner(System.in); 
-     System.out.println("enter number of rows:");
-     int n=input.nextInt();
-     input.close();
-     printPattern(n);
-   }  
-   public static void printPattern(int n){
-        int star=1;
-        int space=n-1;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=space;j++){
-            System.out.print("   ");
-        }
-        for(int j=1;j<=star;j++){
-            System.out.print("+  ");
-        }
-    
-        space-=1;
-        star+=1;
-        System.out.println();
-    }
 
-   }
+public class Q60 {
+public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    System.err.println("Enter no of Rows ");
+    int n=input.nextInt();
+    printPattern(n);
+    input.close();
+}
+    static void printPattern(int n){
+        int space=n-1;
+        
+        for(int i=1;i<=n;i++){
+            int num=i;
+            for (int j = 1; j<=space; j++) {
+                System.out.print("   ");
+            }
+            for(int j =1;j<=i;j++)//till i beacause 2nd row has 2 no. and 3rd has 3 no. and so now no need to trace till nth col
+            {
+                System.out.printf("%3d", num--);
+                
+            }
+            space--;
+            System.out.println();
+        }
+    }
 }
