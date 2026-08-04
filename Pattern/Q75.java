694d3b57a76dcb6f6ebb3d6065d575d569fbe802
Yashfulara <trollj9032@gmail.com>
2026-08-04 22:33:05 +0530

    88 questions done

---
+++ Pattern/Q75.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Q75 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    System.out.print("Enter number of rows : ");
+        int n = input.nextInt();
+        patternPrint(n);
+        input.close();
+
+    }
+    static void patternPrint(int n){
+        int space=n-1;
+        int num=1;
+        for(int i=1;i<=n;i++){
+            
+            for(int j=1;j<=space;j++){
+                System.out.print("   ");
+
+
+            }
+            for(int j=1;j<=i;j++){
+                System.out.printf("%6d",num++);
+
+            }
+            space--;
+            
+            System.out.println();
+        }
+        
+
+    }
+}
