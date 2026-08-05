4e0e1c69648335a1dd0a2278facb8b4d53660522
Yashfulara <trollj9032@gmail.com>
2026-08-05 13:45:12 +0000

    Aug 5, 2026, 7:15 PM

---
+++ Pattern/Q100.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+public class Q100{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter no of rows :");
+        int n= input.nextInt();
+        printPattern(n);
+        input.close();
+    }
+    static void printPattern(int n){
+        int space=n-1;
+        int patternSize=1;
+        
+        for (int i = 1; i <= n; i++) {
+            int num=i;
+            for (int j = 1; j<=space; j++) {
+                System.out.print("   ");
+            }
+            for (int j = 1; j <=patternSize; j++) {
+                if (j<(patternSize/2)+1) {
+                    System.out.printf("%3d",num++);
+                } else {
+                    System.out.printf("%3d",num--);
+                }
+            }
+            
+            space--;
+            patternSize+=2;
+            System.out.println();
+
+            
+        }
+    }
+
+}
