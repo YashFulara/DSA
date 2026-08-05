d6ca056836dc9d09a5ce8548f240ae5153eb24d2
Yashfulara <trollj9032@gmail.com>
2026-08-05 17:06:32 +0000

    Aug 5, 2026, 10:36 PM

---
+++ Pattern/Q110_imp.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Q110_imp {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter no of rows ");
+        int n= input.nextInt();
+        printPattern(n);
+        input.close();
+    }
+    static void printPattern(int n){
+        int space=n-1;
+        int patternSize=1;
+        
+        for (int i = 1; i <=n; i++) {
+            int num=1;
+            for (int j = 1; j <=space; j++) {
+                System.out.print("  ");
+            }
+            for (int j =1; j <=patternSize; j++) {
+                if(j<patternSize/2+1)
+                {
+                    System.out.print((char)(num+64)+" ");
+                    num++;
+                }else{
+                    System.out.print((char)(num+64)+" ");
+                    num--;
+                }
+            }
+            space--;
+            patternSize+=2;
+            System.out.println();
+
+            
+        }
+    }
+}
