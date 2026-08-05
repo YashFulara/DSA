ca0c4b623bc4020381164ca18a95dee24c4b4f04
Yashfulara <trollj9032@gmail.com>
2026-08-05 15:39:54 +0000

    Aug 5, 2026, 9:09 PM

--- Pattern/Q105.java
+++ Pattern/Q105.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q104 {
+public class Q105 {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.println("Enter no of rows ");
@@ -10,7 +10,7 @@ public class Q104 {
     static void printPattern(int n){
         int space=n-1;
         int patternSize=1;
-        int num=65;//starting num from 65 here only cause int 65 in char holds A ive just tried it for this question not recommended though.
+        int num=1;
         for (int i = 1; i <=n; i++) {
             for (int j = 1; j <=space; j++) {
                 System.out.print("  ");
