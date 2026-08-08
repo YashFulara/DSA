dbfc95f4091f3bcfa01dd4652be2f11b01f89902
Yashfulara <trollj9032@gmail.com>
2026-08-09 01:53:55 +0530

    pascal triangle one done

--- Pattern/Q265_PasclTringle.java
+++ Pattern/Q265_PasclTringle.java
@@ -1,3 +1,30 @@
-public class Q265_PasclTringle {
+import java.util.Scanner;
+class Q265_PascalTriangle{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter no. of rows ");
+        int n=input.nextInt();
+        input.close();
+        printPascalTriangle(n);
+    }
+    static void printPascalRow(int n){//special thing about pascal triangle is that for combination n and i are equal (in the formula of nCr)
+        int comb=1;//comb represents combination
+        System.out.print(comb+"   ");
+        for (int i = 0; i <n; i++) {//we can think of i variable as positon(where number will be printed)here.
+            comb=comb*(n-i)/(i+1);
+            System.out.print(comb+"   ");
+        }
+    }
+    static void printPascalTriangle(int n){
+        int space=n;
+        for (int i = 0; i <=n; i++) {//we think of i variable as row here
+            for (int j = 1; j <=space; j++) {
+                System.out.print("  ");
+            }
+            printPascalRow(i);
+            System.out.println();
+            space--;
+        }
+    }
     
 }
