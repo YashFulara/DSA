58f1e1212867174f0a796c22ba2bb848293afa52
Yashfulara <trollj9032@gmail.com>
2026-08-10 23:57:07 +0530

    225 questions done

---
+++ Pattern/Q221.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Q221 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter no of rows ");
+        int n= input.nextInt();
+        input.close();
+        if (n%2==1) {
+            printPattern(n);
+        } else {
+            System.out.println("Please enter odd no. ");
+        }
+    }
+    static void printPattern(int n){
+        int start=1;
+        int end=n;
+        
+        for (int i = 1; i <=n; i++) {
+            
+            for (int j = 1; j <=n; j++) {
+                if (j<=start||j>=end) {
+                    System.out.printf("%3s",(char)(j+64));
+                } else {
+                    System.out.printf("%3s","");
+                }
+                
+            }
+            if (i<n/2+1) {
+                start++;
+                end--;
+
+            } else {
+                start--;
+                end++;
+            }
+            System.out.println();
+            
+        }
+    }
+}
