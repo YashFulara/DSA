1f2391d67832be38e88e4bd82f03ea70cd297665
Yashfulara <trollj9032@gmail.com>
2026-08-13 18:32:01 +0000

    Aug 14, 2026, 12:02 AM

--- Pattern/Q234.java
+++ Pattern/Q234.java
@@ -1,13 +1,13 @@
 public class Q234 {
     public static void main(String[] args) {
         int n=Integer.parseInt(IO.readln("Enter no of sequence"));
-
+        printPattern(n);
     }
 static void printPattern(int n){
     int num=1;
     for (int i = 1; i <=n; i++) {
         System.out.print((num)+", ");
-        
+        num+=2*i;
     }
 
 }
