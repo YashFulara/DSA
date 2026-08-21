3176320e3471217c23f1375c4c064d64fb608da8
Yashfulara <trollj9032@gmail.com>
2026-08-21 23:31:17 +0530

    250 question done

--- Pattern/Q250.java
+++ Pattern/Q250.java
@@ -9,7 +9,7 @@ class Q250{
         printPattern(n);
     }
     static void printPattern(int n){
-       int num=3*n;
+       int num=n*(n+1)/2;
         for (int i = 1; i <=n; i++) {
            
             for (int j = 1; j <=i; j++) {
