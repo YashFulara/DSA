7ba93ef93579f209e4549608be6980228aa30006
Yashfulara <trollj9032@gmail.com>
2026-08-25 22:57:00 +0530

    stared recursion

---
+++ Q1.java
@@ -0,0 +1,13 @@
+public class Q1 {
+    public static void main(String[] args) {
+        int n=1;
+        getNumbers(n);
+    }
+    public static int getNumbers(int n){
+        if (n>10) {
+            return 0;
+        }
+        System.out.println(n);
+        return getNumbers(n+1);
+    }
+}
