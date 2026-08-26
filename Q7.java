2d43be3317a85d0471f481544d68743a8fd24bb6
Yashfulara <trollj9032@gmail.com>
2026-08-26 23:57:57 +0530

    Q7 done

---
+++ Q7.java
@@ -0,0 +1,12 @@
+public class Q7 {
+    public static void main(String[] args) {
+        int n=Integer.parseInt(IO.readln("Enter no for which you want to calculate the factorial "));
+        System.out.println("Factorial of "+n+" is "+getFact(n) );
+    }
+    public static int getFact(int n){
+        if(n==0){
+            return 1;
+        }
+        return n*getFact(n-1);
+    }
+}
