e29f4cfe843234f3442784585ca60efe98c6d3f2
Yashfulara <trollj9032@gmail.com>
2026-08-26 23:50:47 +0530

    Q5 done

---
+++ Q5.java
@@ -0,0 +1,14 @@
+public class Q5 {
+    public static void main(String[] args) {
+        int n=1;
+       int value=getSquare(n);
+       System.out.println("Square is "+ value);
+    }
+    public static int getSquare(int n){
+        if(n>100){
+            return 0;
+            
+        }
+        return n*n+getSquare(n+1);
+    }
+}
