1a276ebac54f880571f720687e745a9a20e42c0b
Yashfulara <trollj9032@gmail.com>
2026-08-30 17:30:46 +0000

    Aug 30, 2026, 11:00 PM

--- WhileLoop1/Q6.java
+++ WhileLoop1/Q6.java
@@ -1,5 +1,24 @@
 package WhileLoop1;
 
 public class Q6 {
-    
+    public static void main(String[] args) {
+        int n=Integer.parseInt(IO.readln("Enter no of bulbs: "));
+        System.out.println(bulbSwitcher(n));
+    }
+    static int bulbSwitcher(int n){
+        int countOn=n;
+        int countOff=0;
+        int pos=1;
+        while (pos<=n) {
+            if (pos<n%2) {
+                countOn+=pos;
+                pos++;
+            }
+            else{
+                countOn+=1;
+            }
+           
+        }
+        return countOn;
+    }
 }
