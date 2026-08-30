5d1ce1c5d42c2087324a1fa1b68f4e20007ccc3d
Yashfulara <trollj9032@gmail.com>
2026-08-30 17:55:07 +0000

    Aug 30, 2026, 11:25 PM

--- WhileLoop1/Q10.java
+++ WhileLoop1/Q10.java
@@ -1,5 +1,11 @@
 package WhileLoop1;
 
 public class Q10 {
-    
+    public static void main(String[] args) {
+        int n=1;
+        while (n%2==0) {
+            System.out.println(n);
+            n++;
+        }
+    }
 }
