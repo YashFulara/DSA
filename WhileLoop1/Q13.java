9546b911eacf8096b031e5f6958e5f5dd86a0bfe
Yashfulara <trollj9032@gmail.com>
2026-08-30 18:20:45 +0000

    Aug 30, 2026, 11:50 PM

--- WhileLoop1/Q13.java
+++ WhileLoop1/Q13.java
@@ -1,5 +1,13 @@
 package WhileLoop1;
 
 public class Q13 {
-    
+    public static void main(String[] args) {
+        int n=1;
+    int count=0;
+    while (n<=100) {
+        if (n%7==0) {
+            System.out.println(n);
+        }
+    }
+    }
 }
