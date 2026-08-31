b6efe24d440e2e63ace4c3f7c37f4d04d723ea32
Yashfulara <trollj9032@gmail.com>
2026-08-31 17:20:43 +0000

    Aug 31, 2026, 10:50 PM

---
+++ WhileLoop1/Q19.java
@@ -0,0 +1,10 @@
+package WhileLoop1;
+
+public class Q19 {
+    public static void main(String[] args) {
+        int n=Integer.parseInt(IO.readln("Enter the no to check if its prime or not "));
+        while (n%2==1) {
+            
+        }
+    }
+}
