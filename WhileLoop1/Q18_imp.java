3f25c1fcd61ffb5084fe0a956fc59b7aa4cb667c
Yashfulara <trollj9032@gmail.com>
2026-08-31 16:55:17 +0000

    Aug 31, 2026, 10:25 PM

--- WhileLoop1/Q18_imp.java
+++ WhileLoop1/Q18_imp.java
@@ -1,17 +1,17 @@
 package WhileLoop1;
 public class Q18_imp {
 public static void main(String[] args) {
-    int n=Integer.parseInt(IO.readln("Enter the no to print and  count of  its factors "));
+    int n=Integer.parseInt(IO.readln("Enter the no to print and count of  its factors "));
     int factor=1;
     int count=0;
     while (factor<=n) {
         if (n%factor==0) {
-            System.out.print("Factors are "+factor+"  ");
+            System.out.print(factor+"  ");
             count++;
         }
         factor++;
     }
-    System.out.println("Total Factors are :"+count);
+    System.out.println("\nTotal Factors are :"+count);
 }
     
 }
