7141ebba026cf3c9c64db337048e56d126649188
Yashfulara <trollj9032@gmail.com>
2026-08-31 16:49:30 +0000

    Aug 31, 2026, 10:19 PM

---
+++ WhileLoop1/Q17_imp.java
@@ -0,0 +1,17 @@
+package WhileLoop1;
+public class Q17_imp {
+public static void main(String[] args) {
+    int n=Integer.parseInt(IO.readln("Enter the no to print count of  its factors "));
+    int factor=1;
+    int count=0;
+    while (factor<=n) {
+        if (n%factor==0) {
+            // System.out.print(factor+"  ");
+            count++;
+        }
+        factor++;
+    }
+    System.out.println("Total Factors are :"+count);
+}
+    
+}
