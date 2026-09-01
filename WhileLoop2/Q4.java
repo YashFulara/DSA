aadc27b43deedc3d2d31730ee07bc838cb73edf8
Yashfulara <trollj9032@gmail.com>
2026-09-01 16:48:46 +0000

    Sep 1, 2026, 10:18 PM

---
+++ WhileLoop2/Q4.java
@@ -0,0 +1,15 @@
+package WhileLoop2;
+
+public class Q4 {
+ public static void main(String[] args) {
+    int n=1;
+    int sum=0;
+    while (n<=100) {
+        if(n%2==1){
+            sum+=n;
+        }
+        n++;
+    }
+    System.out.println(sum);
+ }   
+}
