3823de930f44e7bf853ec75c7715d1365d76ffb3
Yashfulara <trollj9032@gmail.com>
2026-09-03 16:13:15 +0000

    Sep 3, 2026, 9:43 PM

---
+++ WhileLoop2/Q15.java
@@ -0,0 +1,14 @@
+package WhileLoop2;
+
+public class Q15 {
+    public static void main(String[] args) {
+        int n=1;
+        long sum=0;
+        while (n<=100) {
+            sum+=n^2*(n+1);
+            n++;
+            
+        }
+        System.out.println(sum);
+    }
+}
