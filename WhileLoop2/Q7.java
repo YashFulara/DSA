8265a5c5113f92233b87c3f6302240200241d480
Yashfulara <trollj9032@gmail.com>
2026-09-03 12:22:41 +0000

    Sep 3, 2026, 5:52 PM

---
+++ WhileLoop2/Q7.java
@@ -0,0 +1,13 @@
+package WhileLoop2;
+
+public class Q7 {
+   public static void main(String[] args) {
+    int n=1;
+    int sum=0;
+    while (n<=100) {
+        sum+= n*n;
+        n++;
+    }
+    System.out.println(sum);
+   } 
+}
