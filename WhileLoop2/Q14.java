533773eeb782d6bd6fe171ae3091da12dca9ddd5
Yashfulara <trollj9032@gmail.com>
2026-09-03 16:10:37 +0000

    Sep 3, 2026, 9:40 PM

--- WhileLoop2/Q14.java
+++ WhileLoop2/Q14.java
@@ -3,7 +3,7 @@ package WhileLoop2;
 public class Q14 {
     public static void main(String[] args) {
         int n=1;
-        int sum=0;
+        long sum=0;
         while (n<=100) {
             sum+=n*(n+1)*(n+1);
             n++;
