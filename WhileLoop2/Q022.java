583247502967abfb56c3471e0fdd259e2c5cac51
Yashfulara <trollj9032@gmail.com>
2026-09-11 18:25:38 +0000

    Sep 11, 2026, 11:55 PM

--- WhileLoop2/Q022.java
+++ WhileLoop2/Q022.java
@@ -1,5 +1,16 @@
 package WhileLoop2;
 
 public class Q022 {
-    
+    public static void main(String[] args) {
+        int n=Integer.parseInt(IO.readln("Enter no. "));
+        int fact=1;
+        if (n==1) {
+            fact=1;
+        }
+        while (n>1) {
+            fact+=n*(n-1);
+            n--;
+        }
+        System.out.println(fact);
+    }
 }
