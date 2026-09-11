d9c563dc4d2992e3f8c5c9ef93cdf955a90ef6e5
Yashfulara <trollj9032@gmail.com>
2026-09-11 17:02:25 +0000

    Sep 11, 2026, 10:32 PM

--- WhileLoop2/Q17.java
+++ WhileLoop2/Q17.java
@@ -1,5 +1,13 @@
 package WhileLoop2;
 
 public class Q17 {
-    
+    public static void main(String[] args) {
+        double sum=0;
+        double n=100;
+        while (n<=100) {
+            if (n%2==0) {
+                sum+=1/n;
+            }
+        }
+    }
 }
