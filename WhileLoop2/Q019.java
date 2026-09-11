b920a31ff0b0fe8ca73f08a5ed2173bf2fa8dbce
Yashfulara <trollj9032@gmail.com>
2026-09-11 18:16:30 +0000

    Sep 11, 2026, 11:46 PM

--- WhileLoop2/Q019.java
+++ WhileLoop2/Q019.java
@@ -2,6 +2,12 @@ package WhileLoop2;
 
 public class Q019 {
     public static void main(String[] args) {
-        
+        double n=1;
+        double sum=0;
+        while (n<=100) {
+           sum+=1/(n*n);
+           n++; 
+        }
+        System.out.println(sum);
     }
 }
