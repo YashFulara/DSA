3ade65ee859955a16ca048578b7da429abfc2b0a
Yashfulara <trollj9032@gmail.com>
2026-09-21 17:24:49 +0000

    Sep 21, 2026, 10:54 PM

--- WhileLoop3/Q037.java
+++ WhileLoop3/Q037.java
@@ -5,21 +5,23 @@ public class Q037 {
         int num=Integer.parseInt(IO.readln("Enter no "));
         System.out.println(isSameAfterReversals(num));
     }
-    public static boolean isSameAfterReversals(int num) {
+     public static
+      boolean isSameAfterReversals(int num) {
        int n=num;
-       int rev1=getDoubleReverse(num);
-       if (rev1==n) {
-            return true;
-       }
-       return false;
-    }
-    public static int  getDoubleReverse(int num){
-        int rev1=0;
+       int rev1=0;
+       int rev2=0;
         while(num>0){
             rev1=rev1*10+num%10;
             num=num/10;
         }
-        getDoubleReverse(rev1);
-        return rev1;
+        while(rev1>0){
+            rev2=rev2*10+rev1%10;
+            rev1=rev1/10;
         }
+        if(n==rev2)
+            return true;
+        return false;
+        
+    }
+    
 }
